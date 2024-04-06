87f5451fa6e29d41040b846ea23370860fccbf45
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-04-06 17:39:49 +0530

    final file

---
+++ script.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('.scaleUp',
+{ scale: 0.85 ,  duration: 1000 });
+
+document.getElementById('copyBtn').addEventListener('click', function() {
+    // Create a temporary textarea element
+    var tempTextarea = document.createElement('textarea');
+    tempTextarea.value = 'cdn  here';
+    document.body.appendChild(tempTextarea);
+    // Select the text inside the textarea
+    tempTextarea.select();
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextarea);
+    // Inform the user that the text has been copied
+    alert('Cdn coppied successfully.');
+  });
